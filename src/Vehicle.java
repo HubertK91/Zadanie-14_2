e93e72ac392a097ba192416cc2e75c545ce455e9
Hubert Kowalski <hubert160491@gmail.com>
2021-02-19 13:39:19 +0100

    Rozwiązanie zadania

---
+++ src/Vehicle.java
@@ -0,0 +1,29 @@
+class Vehicle {
+    String type;
+    String brand;
+    String model;
+    int year;
+    int mileage;
+    String vinNumber;
+
+    public Vehicle(String type, String brand, String model, int year, int mileage, String vinNumber) {
+        this.type = type;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.mileage = mileage;
+        this.vinNumber = vinNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "type='" + type + '\'' +
+                ", brand='" + brand + '\'' +
+                ", model='" + model + '\'' +
+                ", year=" + year +
+                ", mileage=" + mileage +
+                ", vinNumber='" + vinNumber + '\'' +
+                '}';
+    }
+}
